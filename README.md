9b6cdcb6bbc0e50285d82b6630016e8942f1549a
QianChen <qianchenzhumeng@example.com>
2015-01-18 15:18:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,110 @@
+onscreenkeyboard
+================
+
+
+This is a framebuffer on-screen keyboard proof of concept
+for text console or X. Might be usefull at boot time for accessibility.
+Currently, you can move or roll the mouse/trackball to the proper key, then hit the left mouse button to send the keystroke to the system, like a real keyboard.
+
+
+```
+ * Copyright (C) 2014 Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
+ *
+ *   osk.c and osk_mouse.c is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version. Check COPYING file.
+```
+
+osk_mouse.c and osk.c source file implements this.
+
+ - No extra libraries. Great for using at boot time :
+```
+	# ldd osk_mouse
+       /usr/lib/arm-linux-gnueabihf/libcofi_rpi.so (0xb6fd0000)
+	   libc.so.6 => /lib/arm-linux-gnueabihf/libc.so.6 (0xb6e92000)
+	   /lib/ld-linux-armhf.so.3 (0xb6fde000)
+```
+ - on scren keyboard using framebuffer. Ok for console
+ - it sends keystrokes using uinput
+
+Check the videos to know the idea and proof of concept working :
+
+http://youtu.be/TwYQzoEr5Lw
+
+http://youtu.be/zHOJGq4hkpg
+
+
+How to use
+==========
+
+make
+modprobe uinput
+# run on text console
+Usage:
+ 	sudo ./osk_mouse [Port]
+Example:
+ 	sudo ./osk_mouse /dev/input/event0
+
+The 'E' symbol is ENTER. 'B' backspace.
+
+```
+
+Keyboard version
+----------------
+
+```
+# Be sure that /dev/input/event0 is your real keyboard, 
+# you need it to choose keys from onscreen keyboard
+
+make
+modprobe uinput
+# run on text console or X
+./osk &  #YOU NEED TO BE ROOT
+
+# Use "END" key from your keyboard to choose a key from the
+# on screen keyboard. Otherwise your console or bash
+# could get two keys, the one you pressed and the choosed
+# from the on screen keyboard.
+
+# When you want to exit, you should press your "END" key in your
+# keyboard when green box dissappear from keyboard on screen
+# for two seconds. That happens every time it reachs to the 
+# end of the on screen keyboard keys
+
+```
+
+You will see the on screen keyboard on console on top of framebuffer.
+There is green box which go from "a" to "z" delaying one second
+on every letter. YOu have "E" for ENTER after "z"
+When you press the "End" key on your real keyboard, you choose
+the letter where green box is, and the program sends a 
+proper keystrokes to the system using uinput.
+
+For example, if green box is under "l", and you press the "END" key
+on your keyboard, you will see the "l" showed on console, like
+if you would had pressed the "l" key.
+
+
+
+Old ideas
+=========
+
+The idea is :
+
+ - draw a keyboard on framebuffer
+ - someway to choose a key from that keyboard
+ - send keystrokes to the Linux system as a real keyboard
+
+For all the 3 ideas we already have examples to start.
+
+If we ldd all the binaries for these examples we 
+verify that these use just the below libraries :
+        linux-gate.so.1 (0xb7785000)
+        libc.so.6 => /lib/i386-linux-gnu/i686/cmov/libc.so.6 (0xb75a2000)
+        /lib/ld-linux.so.2 (0xb7786000)
+
+which is that we want, in order to have on-screen keyboard ready on boot time.
+
+
+The image.ppm is the keyboard image. We did it with Gimp and exported as raw ppm.
---
+++ osk_mouse.c
@@ -0,0 +1,358 @@
+
+/*
+ * osk_mouse.c : framebuffer on-screen keyboard. Mouse or trackball version
+ * 
+ * Copyright (C) 2014 Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
+ *
+ * Based on : http://thiemonge.org/getting-started-with-uinput
+ * and www.cubieforums.com/index.php?topic=33.0
+ * and fb_drawimage() from https://svn.mcs.anl.gov/repos/ZeptoOS/trunk/BGP/packages/busybox/src/miscutils/fbsplash.c 
+ *
+ *   osk_mouse.c is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *    You should have received a copy of the GNU Library General Public
+ *    License along with this software (check COPYING); if not, write to the Free
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <linux/input.h>
+#include <linux/ioctl.h>
+#include <linux/uinput.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+#define UMBRAL 10
+
+
+
+unsigned char abc[] = {KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_ENTER, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_ENTER, 0, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_COMMA, KEY_DOT, KEY_DOT, KEY_DOT, KEY_DOT, KEY_BACKSPACE};
+
+
+
+
+/*
+ *	Draw image from PPM file
+ */
+static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bpp, int length, int xres) 
+{
+	char head[256];
+	char s[80];
+	FILE *theme_file;
+	unsigned char *pixline;
+	unsigned i, j, width, height, line_size;
+
+	memset(head, 0, sizeof(head));
+	theme_file = fopen(filename, "r");
+
+	/* parse ppm header */
+	while (1) {
+		if (fgets(s, sizeof(s), theme_file) == NULL) {
+			printf("bad PPM file '%s'", filename);
+			exit(1);
+		}
+
+		if (s[0] == '#')
+			continue;
+
+		if (strlen(head) + strlen(s) >= sizeof(head)) {
+			printf("bad PPM file '%s'", filename);
+			exit(1);
+		}
+
+		strcat(head, s);
+		if (head[0] != 'P' || head[1] != '6') {
+			printf("bad PPM file '%s'", filename);
+			exit(1);
+		}
+
+		/* width, height, max_color_val */
+		if (sscanf(head, "P6 %u %u %u", &width, &height, &i) == 3)
+			break;
+		/* TODO: i must be <= 255! */
+	}
+
+	line_size = width*3;
+
+	long int location = 0;
+	pixline = malloc(line_size);
+	for (j = 0; j < height; j++) {
+		unsigned char *pixel = pixline;
+
+		if (fread(pixline, 1, line_size, theme_file) != line_size) {
+			printf("bad PPM file '%s'", filename);
+			exit(1);
+		}
+
+		for (i = 0; i < width; i++) {
+			pixel += 3;
+			location = ((xres-width)+i+xo) * (bpp/8) + (j+yo) * length;
+
+			*(fbp + location) = (((unsigned)pixel[0]));
+			*(fbp + location + 1) = (((unsigned)pixel[1]));
+			*(fbp + location + 2) = (((unsigned)pixel[2]));
+			*(fbp + location + 3) = -1;      /* No transparency */
+		}
+	}
+	free(pixline);
+	fclose(theme_file);
+}
+
+
+
+int send_event(int fd, __u16 type, __u16 code, __s32 value)
+{
+    struct input_event event;
+
+    memset(&event, 0, sizeof(event));
+    event.type = type;
+    event.code = code;
+    event.value = value;
+
+    if (write(fd, &event, sizeof(event)) != sizeof(event)) {
+        fprintf(stderr, "Error on send_event\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static void fb_drawbox(char *fbp, int x1, int y1, int x2, int y2, int bpp, int length)
+{
+	int x, y;
+	long int location = 0;
+
+     		for (y = y1; y < y2; y++)
+       		for (x = x1; x < x2; x++) {
+
+			location = (x) * (bpp/8) + (y) * length;
+
+			*(fbp + location) = 100;
+			*(fbp + location + 1) = 100;
+			*(fbp + location + 2) = 200;
+			*(fbp + location + 3) = -1;      /* No transparency */
+
+			}
+
+}
+
+
+int main(int argc, char **argv) {
+
+	int fbfd = 0;
+	char strPort[64] = { 0 };
+	struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	long int screensize = 0;
+	char *fbp = 0;
+
+	if(argc == 1 || argc > 2 ) {
+        	printf(" \nUsage:\n \tosk_mouse [Port]\n");
+        	printf("Example:\n \tosk_mouse /dev/input/event0\n\n");
+        	return 0;
+	}
+	
+	sprintf( strPort, "%s", argv[1] );
+    	printf("Event Port = %s\n", strPort);
+
+	/* uinput init */
+	int i,fd;
+	struct uinput_user_dev device;
+	memset(&device, 0, sizeof device);
+
+	fd=open("/dev/uinput",O_WRONLY);
+	strcpy(device.name,"test keyboard");
+
+	device.id.bustype=BUS_USB;
+	device.id.vendor=1;
+	device.id.product=1;
+	device.id.version=1;
+
+	if (write(fd,&device,sizeof(device)) != sizeof(device))
+	{
+		fprintf(stderr, "error setup\n");
+	}
+
+	if (ioctl(fd,UI_SET_EVBIT,EV_KEY) < 0)
+		fprintf(stderr, "error evbit key\n");
+
+	for (i=0;i<50;i++)
+		if (ioctl(fd,UI_SET_KEYBIT, abc[i]) < 0)
+			fprintf(stderr, "error evbit key\n");
+
+	if (ioctl(fd,UI_SET_EVBIT,EV_REL) < 0)
+		fprintf(stderr, "error evbit rel\n");
+
+	for(i=REL_X;i<REL_MAX;i++)
+		if (ioctl(fd,UI_SET_RELBIT,i) < 0)
+			fprintf(stderr, "error setrelbit %d\n", i);
+
+	if (ioctl(fd,UI_DEV_CREATE) < 0)
+	{
+		fprintf(stderr, "error create\n");
+	}
+
+	sleep(2);
+	/* end uinput init */
+
+
+	/* init framebuffer */
+	/* Open the file for reading and writing */
+	fbfd = open("/dev/fb0", O_RDWR);
+	if (fbfd == -1) {
+		perror("Error: cannot open framebuffer device");
+		exit(1);
+	}
+	printf("The framebuffer device was opened successfully.\n");
+
+	/* Get fixed screen information */
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+		perror("Error reading fixed information");
+		exit(2);
+	}
+
+	/* Get variable screen information */
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+		perror("Error reading variable information");
+		exit(3);
+	}
+
+	printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+
+	/* Figure out the size of the screen in bytes */
+	screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+
+	/* Map the device to memory */
+	fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
+                        fbfd, 0);
+	if ((int)fbp == -1) {
+		perror("Error: failed to map framebuffer device to memory");
+		exit(4);
+	}
+	printf("The framebuffer device was mapped to memory successfully.\n");
+
+	/* end init framebuffer */
+
+
+
+	/* init mouse */
+	int fdm;
+	struct input_event evm;
+
+	if ((fdm = open(strPort, O_RDONLY | O_NONBLOCK)) == -1) {
+		printf("Device open ERROR\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int ret, xp=0, yp=0, rkp=0, ckp=0;
+	unsigned char button,bLeft,bMiddle,bRight;
+	char xm,ym;
+	int absolute_x,absolute_y;
+	/* end init mouse */
+
+	int pressed=0;
+	int pressedk=0;
+
+	int j;
+
+	while(1) {
+
+		fb_drawimage("image_mouse.ppm", fbp, vinfo.xoffset, vinfo.yoffset, vinfo.bits_per_pixel, finfo.line_length, vinfo.xres);
+
+
+		/* check mouse */
+
+ 		ret=read(fdm, &evm, sizeof(struct input_event));
+		if (ret!=-1) {
+			if(evm.type == EV_REL) {
+				if(evm.code == REL_X) {
+					if(evm.value > 0) xp++;
+					else if(evm.value < 0) xp--;
+				}
+				else if(evm.code == REL_Y) {
+					if (evm.value > 0) yp++;
+					else if(evm.value < 0) yp--;
+				}
+
+				if (xp==UMBRAL) {
+					xp=0;
+					ckp++;
+					if (ckp>9) ckp=9;
+				} else if (xp==-UMBRAL) {
+					xp=0;
+					ckp--; if (ckp<0) ckp=0;
+				}
+
+				if (yp==UMBRAL) {
+					yp=0;
+					rkp++; if (rkp>4) rkp=4;
+				} else if (yp==-UMBRAL) {
+					yp=0;
+					rkp--; if (rkp<0) rkp=0;
+				}
+				//printf("ckp:%d\nrkp%d\n", ckp, rkp);
+			}
+			absolute_x+=xm;
+			absolute_y-=ym;
+			if (evm.type == EV_KEY && evm.value == 1) {
+				pressed=1;
+				pressedk=(rkp*10)+ckp;
+				//printf("pressedk:%d\n", pressedk);
+			}
+			else if (evm.type == EV_KEY && evm.value == 0) {	
+				if (abc[pressedk] == 0) break;
+				pressed=0;
+				send_event(fd, EV_KEY, abc[pressedk], 1);
+				send_event(fd, EV_SYN, SYN_REPORT, 0);
+				send_event(fd, EV_KEY, abc[pressedk], 0);
+				send_event(fd, EV_SYN, SYN_REPORT, 0);
+			}
+
+		}
+
+		/* the whole mess to draw the pointer/cursor */
+		int x1, x2, y1, y2;
+		int anchoi = 240;	/* width of keyboard image */
+		int altoi = 125;	/* height of keyboard image */
+		int nro_columnas=10;
+		int nro_filas=5;
+
+		x1=(vinfo.xres-anchoi)+((anchoi/nro_columnas)*(ckp));
+		x2=x1+(anchoi/nro_columnas);
+		y1=(altoi/nro_filas)*(rkp);
+		y2=y1+4;
+
+		fb_drawbox(fbp, x1, y1, x2, y2, vinfo.bits_per_pixel, finfo.line_length);
+
+		y1=(altoi/nro_filas)*(rkp)+(altoi/nro_filas-4); // The "4" here is a ugly hardcoded number of pixels less
+		y2=y1+4;		/* 4 is the height of the line */
+
+		fb_drawbox(fbp, x1, y1, x2, y2, vinfo.bits_per_pixel, finfo.line_length);
+
+		usleep(1000);
+
+	}
+
+	close(fd);
+	close(fdm);
+
+	munmap(fbp, screensize);
+	close(fbfd);
+
+	return 0;
+}
+
